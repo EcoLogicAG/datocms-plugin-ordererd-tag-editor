2faaa9eaef99876f0f76276931686ee597922c8d
Markus Huggler <markus.huggler@ecologic.ch>
2019-04-02 00:00:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
-# DatocmsPluginOrderedTagEditor
+# DatoCMS Ordered Tag Editor
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.5.
+Tag editor which stores its values in a sorted way
+
+## Screenshot
+
+![Editor](doc/editor.png?raw=true "Editor")
+
+![Configuration](doc/configuration.png?raw=true "Configuration")
+
+## Configuration
+
+The plugin stores the values in ascending order per default, you can change that in the field settings.
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+Run `npm run build` to build into `dist`
 
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import DatoCmsPlugin from 'datocms-plugins-sdk';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+interface IOption {
+    sortOrderDescending: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'datocms-plugin-ordered-tag-editor';
+export class AppComponent implements OnInit {
+    @ViewChild('tagInput') tagInputRef: ElementRef;
+    public tags: string[] = [];
+    public form: FormGroup;
+
+    public fieldName: string;
+    public options: IOption = <IOption> { sortOrderDescending: false };
+    public plugin: any;
+
+    constructor(private fb: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            tag: [undefined],
+        });
+        DatoCmsPlugin.init((plg) => {
+            this.plugin = plg;
+            this.plugin.startAutoResizer();
+
+            this.fieldName = this.plugin.field.attributes.label;
+
+            this.tags = this.plugin.getFieldValue(this.plugin.fieldPath).split(',');
+
+            this.options.sortOrderDescending = this.plugin.parameters.instance.sortOrderDescending;
+        });
+    }
+
+    focusTagInput() {
+        this.tagInputRef.nativeElement.focus();
+    }
+
+    removeTag(tag?: string) {
+        if (tag) {
+            const index = this.tags.indexOf(tag, 0);
+            if (index > -1) {
+                this.tags.splice(index, 1);
+                this.plugin.setFieldValue(this.plugin.fieldPath, this.tags.join(','));
+            }
+        }
+    }
+
+    addTag(tag: string): void {
+        if (tag.length > 0 && (this.tags.indexOf(tag, 0) === -1)) {
+            this.tags.push(tag.trim());
+            this.tags.sort();
+            if (this.options.sortOrderDescending) {
+                this.tags.reverse();
+            }
+            this.plugin.setFieldValue(this.plugin.fieldPath, this.tags.join(','));
+        }
+    }
+
+    onKeyUp(event: KeyboardEvent) {
+        const inputValue: string = this.form.controls.tag.value;
+        if (event.code === 'Backspace' && !inputValue) {
+            this.removeTag();
+            return;
+        } else {
+            if (event.code === 'Comma' || event.code === 'Space' || event.code === 'Enter' || event.code === 'NumpadEnter') {
+                this.addTag(inputValue);
+                this.form.controls.tag.setValue('');
+            }
+        }
+    }
 }
